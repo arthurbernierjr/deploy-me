5a89e72a619e72a8ad412d8824b210889e34a7df
Arthur Bernier <ma2bernier@gmail.com>
2021-04-01 22:39:42 -0400

    Minor Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import Other from './Other';
